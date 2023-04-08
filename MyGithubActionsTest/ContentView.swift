6c95570f90fdc2b26d8dbfe5136e27e4fd3fd390
Scott Bennett <stt.btt@gmail.com>
2023-04-08 17:10:59 -0400

    Initial Commit

---
+++ MyGithubActionsTest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyGithubActionsTest
+//
+//  Created by Scott Bennett on 4/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyGithubActionsTest/MyGithubActionsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGithubActionsTestApp.swift
+//  MyGithubActionsTest
+//
+//  Created by Scott Bennett on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct MyGithubActionsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
